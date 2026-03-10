f17c96674e9d634749f1e57e115ca0aa88f82ca5
vvrc6334 <rcubillosb@gmail.com>
2026-03-10 12:45:49 -0300

    feat: init TalentLab LMS

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  }
+  resolve: { alias: { '@': resolve(__dirname, 'src') } }
 })
